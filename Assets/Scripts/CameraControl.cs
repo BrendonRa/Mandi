451305974e62843f76034078f2539095b02abfca
Brendon Rabelo dos Santos <105553904+BrendonRa@users.noreply.github.com>
2025-05-14 09:50:08 -0300

    update object rotaion

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -5,16 +5,25 @@ using UnityEngine;
 public class CameraControl : MonoBehaviour
 {
     public Transform objectToFollow;
+    public float rotationSpeed = 150.0f;
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
         var delta = objectToFollow.position;
-        transform.position = delta; 
+        transform.position = delta;
+        CamOrbit();
+    }
+
+    private void CamOrbit() {
+        if (Input.GetAxis("Mouse Y") != 0 || Input.GetAxis("Mouse X") != 0) {
+            float verticalInput = Input.GetAxis("Mouse Y") * rotationSpeed * Time.deltaTime;
+            float horizontalInput = Input.GetAxis("Mouse X") * rotationSpeed * Time.deltaTime;
+            transform.Rotate(Vector3.up, horizontalInput, Space.World);
+        }
     }
 }
--- Assets/Scripts/RotationObjects.cs
+++ Assets/Scripts/RotationObjects.cs
@@ -4,23 +4,16 @@ using UnityEngine;
 
 public class RotationObjects : MonoBehaviour
 {
-    public float rotationSpeed = 150.0f;
+    public Transform rotationObject;
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
-    private void LateUpdate()
+    void Update()
     {
-        CamOrbit();
-    }
-    private void CamOrbit() {
-        if (Input.GetAxis("Mouse Y") != 0 || Input.GetAxis("Mouse X") != 0) {
-            float verticalInput = Input.GetAxis("Mouse Y") * rotationSpeed * Time.deltaTime;
-            float horizontalInput = Input.GetAxis("Mouse X") * rotationSpeed * Time.deltaTime;
-            transform.Rotate(Vector3.up, horizontalInput, Space.World);
-        }
+        var rota = rotationObject.rotation;
+        transform.rotation = rota;
     }
 }
