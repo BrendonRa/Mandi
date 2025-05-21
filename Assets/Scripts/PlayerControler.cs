0affc24346ebe208524e79fcf8f79b228c3cacdf
Brendon Rabelo dos Santos <105553904+BrendonRa@users.noreply.github.com>
2025-05-21 14:41:13 -0300

    update movements

--- Assets/Scripts/PlayerControler.cs
+++ Assets/Scripts/PlayerControler.cs
@@ -16,17 +16,23 @@ public class PlayerControler : MonoBehaviour
         float xAxis = Input.GetAxisRaw("Horizontal");
         float zAxis = Input.GetAxisRaw("Vertical");
 
-        moviment.Translate(Vector3.forward * Time.deltaTime * zAxis * moveSpeed);
-        // rb.MovePosition(transform.position + new Vector3(xAxis,0,zAxis) * moveSpeed * Time.deltaTime);
+        //moviment.Translate(Vector3.forward * Time.deltaTime * zAxis * moveSpeed);
+        rb.MovePosition(transform.position + new Vector3(xAxis,0,zAxis) * moveSpeed * Time.deltaTime);
         // rb.MoveRotation(Quaternion.Euler(0f, transform.rotation.y + xAxis * moveSpeed * Time.deltaTime, 0f));
 
-        if(zAxis != 0){
-                anim.SetBool("isWalking", true);
-        }else{
-                anim.SetBool("isWalking", false);
+        if (zAxis != 0 || xAxis != 0)
+        {
+            anim.SetFloat("input_x", xAxis);
+            anim.SetFloat("input_z", zAxis);
+            anim.SetBool("isWalking", true);
+        }
+        else
+        {
+            anim.SetBool("isWalking", false);
         }
 
-        if (Input.GetButtonDown("Fire1")){
+        if (Input.GetButtonDown("Fire1"))
+        {
             anim.SetTrigger("attack");
         }
         /*
