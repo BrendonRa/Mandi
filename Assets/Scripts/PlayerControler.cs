d2364844e9b828ed410ba619b4939af57cf105fb
Brendon Rabelo dos Santos <105553904+BrendonRa@users.noreply.github.com>
2025-05-26 08:44:35 -0300

    Movement System

--- Assets/Scripts/PlayerControler.cs
+++ Assets/Scripts/PlayerControler.cs
@@ -8,22 +8,28 @@ public class PlayerControler : MonoBehaviour
     public Rigidbody rb;
     public Animator anim;
     public float moveSpeed = 5.0f;
-    //private Camera _mainCamera;
-    private SpriteRenderer sprite;
-    public Transform moviment;
+    public float rotationSpeed = 100f;
     void Update()
     {
         float xAxis = Input.GetAxisRaw("Horizontal");
         float zAxis = Input.GetAxisRaw("Vertical");
 
         //moviment.Translate(Vector3.forward * Time.deltaTime * zAxis * moveSpeed);
-        rb.MovePosition(transform.position + new Vector3(xAxis,0,zAxis) * moveSpeed * Time.deltaTime);
+        // rb.MovePosition(transform.position + transform.forward * (zAxis * moveSpeed * Time.deltaTime));
+        // rb.MovePosition(transform.position + transform.right * (xAxis * moveSpeed * Time.deltaTime));
         // rb.MoveRotation(Quaternion.Euler(0f, transform.rotation.y + xAxis * moveSpeed * Time.deltaTime, 0f));
+
+        // Mover para frente/trás
+        transform.Translate(Vector3.forward * zAxis * moveSpeed * Time.deltaTime);
+
+        // Rotacionar para esquerda/direita
+        transform.Rotate(Vector3.up * xAxis * rotationSpeed * Time.deltaTime);
+
+        anim.SetFloat("input_x", zAxis);
+        anim.SetFloat("input_z", xAxis);
 
         if (zAxis != 0 || xAxis != 0)
         {
-            anim.SetFloat("input_x", xAxis);
-            anim.SetFloat("input_z", zAxis);
             anim.SetBool("isWalking", true);
         }
         else
@@ -35,11 +41,5 @@ public class PlayerControler : MonoBehaviour
         {
             anim.SetTrigger("attack");
         }
-        /*
-        Vector3 cameraPosition = _mainCamera.transform.position;
-        cameraPosition.y = transform.position.y;
-        transform.LookAt(cameraPosition);
-        transform.Rotate(0f, 180f, 0f);
-        */
     }
 }
